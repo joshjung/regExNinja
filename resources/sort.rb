2c6d372a5e82672d93a1b192f28d6b5759f7f617
Ty Cobb <tcobb@roundarch.com>
2013-04-06 11:48:04 -0500

    updated source sorter to organize words by frequency and format them as valid json

--- resources/sort.rb
+++ resources/sort.rb
@@ -1,21 +1,47 @@
 require 'set'
 
-CHUNK_SIZE = 1024
-SOURCE_FILE = 'amusements-in-mathematics.txt'
+class BookSorter
 
-File.open(SOURCE_FILE) do |file|
-  
-  word_set = Set.new
-
-  until file.eof?
-    chunk = file.read(CHUNK_SIZE)
-    chunk_word_array = chunk.scan(/[a-zA-Z][a-z]+/)
-    chunk_word_array.each { |word| word_set.add(word.downcase) if word.length > 2 }
+  def initialize(source_file)
+    @chunk_size  ||= 1024
+    @source_file ||= source_file
   end
 
-  word_set_string = word_set.to_a * ', '
-  word_set_string = "[ #{ word_set_string } ]"
+  def sort 
+    File.open(@source_file) do |file|
+      
+      word_set    = Set.new
+      word_counts = Hash.new
 
-  File.open('word_set.txt', 'w') { |file| file.write(word_set_string) }
+      until file.eof?
+        chunk = file.read(@chunk_size)
+        chunk_word_array = chunk.scan(/[a-zA-Z][a-z]+/)
+        
+        chunk_word_array.each do |word|
+          next if word.length < 3 # skip words less than 3 characters
+          
+          word = "\"#{ word.downcase }\"" # format these nicely
+          new_word = word_set.add?(word) # add? returns the set if added, or nil if duplicate
+        
+          word_counts[word] = 0 if new_word
+          word_counts[word] += 1
+        
+        end 
+      end
+
+      ordered_word_set = word_set.to_a.sort do |word1, word2|
+        word_counts[word1] <=> word_counts[word2]
+      end.reverse!
+
+      word_set_string = ordered_word_set * ', '
+      word_set_string = "[ #{ word_set_string } ]"
+
+      File.open('word_set.txt', 'w') { |file| file.write(word_set_string) }
+    
+    end
+  end
 
 end
+
+sorter = BookSorter.new('../amusements-in-mathematics.txt')
+sorter.sort
